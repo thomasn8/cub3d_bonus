dbba51ca5ca7ad2e1097a40a60f3c0886ed2bb18
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-03-14 19:37:25 +0100

    différenciation des faces murales

--- srcs/minimap/player.c
+++ srcs/minimap/player.c
@@ -1,30 +1,29 @@
 #include "../../includes/cub3d.h"
 #include "../../includes/map.h"
 
-void	remove_prev_fov(t_image *map, t_map *m)
-{
-	int	width;
-	int	height;
-	int x;
-	int y;
+// void	remove_prev_fov(t_image *map, t_map *m)
+// {
+// 	int	width;
+// 	int	height;
+// 	int x;
+// 	int y;
 
-	height = -2;
-	while (++height < m->m_size * 2 + 1)
-	{
-		width = -2;
-		while (++width < m->m_size * 2 + 1)
-		{
-			x = m->prev_x + width - m->m_size + M_HALF_PLAYER;
-			y = m->prev_y + height - m->m_size + M_HALF_PLAYER;
-			my_mlx_pixel_put(map, x, y, m->clean_map[y][x]);
-		}
-	}
-}
+// 	height = -2;
+// 	while (++height < m->m_size * 2 + 1)
+// 	{
+// 		width = -2;
+// 		while (++width < m->m_size * 2 + 1)
+// 		{
+// 			x = m->prev_x + width - m->m_size + M_HALF_PLAYER;
+// 			y = m->prev_y + height - m->m_size + M_HALF_PLAYER;
+// 			my_mlx_pixel_put(map, x, y, m->clean_map[y][x]);
+// 		}
+// 	}
+// }
 
 /* redessine entièrement la minimap
 à utiliser lorsqu'on projette les rayons dans la minimap
 la fonction du dessus est + rapide car redessine le minium nécessaire */
-/*
 void	remove_prev_fov(t_image *map, t_map *m)
 {
 	int	width;
@@ -38,7 +37,6 @@ void	remove_prev_fov(t_image *map, t_map *m)
 			my_mlx_pixel_put(map, width, height, m->clean_map[height][width]);
 	}
 }
-*/
 
 void	remove_prev_pos(t_image *map, t_map *m)
 {
