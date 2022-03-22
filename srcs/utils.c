b60543bff8cbf36593fa035ae760e1b2a2c06681
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-03-22 01:10:15 +0100

    modifier get_tex_color pour appliquer effet d'ombre

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,7 +1,6 @@
 #include "../includes/cub3d.h"
 #include "../includes/map.h"
 
-
 float deg_to_rad(int angle)
 {
 	return (angle * PI / 180);
@@ -20,36 +19,15 @@ unsigned int get_color_value(t_image *image, int x, int y)
 	return (*(unsigned int*)ptr);
 }
 
-int	get_t(int trgb)
-{
-	return ((trgb >> 24) & 0xFF);
-}
-
-int	get_r(int trgb)
-{
-	return ((trgb >> 16) & 0xFF);
-}
-
-int	get_g(int trgb)
-{
-	return ((trgb >> 8) & 0xFF);
-}
-
-int	get_b(int trgb)
-{
-	return (trgb & 0xFF);
-}
-
 unsigned int get_tex_color(t_img *tex, int x, int y, float shade)
 {
 	unsigned int	clr;
-	int t;
-	int	r;
-	int	g;
-	int	b;
+	int 			t;
+	int				r;
+	int				g;
+	int				b;
 
 	clr = *(unsigned int*)(tex->addr + (y * tex->ll + x * (tex->bpp / 8)));
-	// return (clr);
 	t = ((clr >> 24) & 0xFF);
 	r = ((clr >> 16) & 0xFF) * shade;
 	g = ((clr >> 8)  & 0xFF) * shade;
