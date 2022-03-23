7a619a10cb351d00b72e557eac62bb9999f69207
deslarzes97 <78907448+deslarzes97@users.noreply.github.com>
2022-03-23 16:38:18 +0100

    modification conflit merge depuis master

--- srcs/mouse.c
+++ srcs/mouse.c
@@ -1,18 +1,6 @@
 #include "../includes/cub3d.h"
 #include "../includes/event.h"
 
-// int	mouse_event(int button, int x, int y, t_game *game)
-// {
-// 	game->mo.m_x = x;
-// 	game->mo.m_y = y;
-// 	game->mo.button = button;
-// 	printf("button = %i x = %i et y = %i\n", game->mo.button, game->mo.m_x, game->mo.m_y);
-// 	printf("x1 = %i et y1 = %i  // x2 = %i et y2 = %i\n", game->p.x1, game->p.y1, game->p.x2, game->p.y2);
-// 	return (0);
-// }
-
-<<<<<<< HEAD
-=======
 int	mouse_press(int button, int x, int y, t_game *game)
 {
 	(void) x;
@@ -29,7 +17,6 @@ int	mouse_release(int button, int x, int y, t_game *game)
 	return (0);
 }
 
->>>>>>> origin/master
 int	mouse_move(int mx, int my, t_game *game)
 {
 	static int	x;
@@ -44,40 +31,9 @@ int	mouse_move(int mx, int my, t_game *game)
 		if ((mx >= 0 && mx < 72) && (my >= 0 && my < 670) && (x - mx) > 0)
 			rotation(game, 'r');
 	}
-<<<<<<< HEAD
-	printf("my = %i mx = %i\n", my, mx);
-=======
 	// printf("my = %i mx = %i\n", my, mx);
->>>>>>> origin/master
 	x = mx;
 	return (0);
 
 	return (0);
 }
-<<<<<<< HEAD
-
-int	mouse_press(int button, int x, int y, t_game *game)
-{
-	(void) x;
-	if (y >= 0)
-	{
-		if (button == M_R)
-			kat_attack(game, 'p');
-		else if (button == M_L)
-			kat_defense(game, 'p');
-	}
-	return (0);
-}
-
-int	mouse_release(int button, int x, int y, t_game *game)
-{
-	(void) x;
-	(void) y;
-	if (button == M_R)
-		kat_attack(game, 'r');
-	else if (button == M_L)
-		kat_defense(game, 'r');
-	return (0);
-}
-=======
->>>>>>> origin/master
