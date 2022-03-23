4f793aa792ea86b8daf888745b757a4cbb59fcf8
deslarzes97 <78907448+deslarzes97@users.noreply.github.com>
2022-03-23 17:04:52 +0100

    partie parsing et mouse.c sont terminées et norminettées

--- srcs/mouse.c
+++ srcs/mouse.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: desa <desa@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 17:03:19 by desa              #+#    #+#             */
+/*   Updated: 2022/03/23 17:03:20 by desa             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/cub3d.h"
 #include "../includes/event.h"
 
@@ -23,7 +35,7 @@ int	mouse_move(int mx, int my, t_game *game)
 
 	game->left = 0;
 	game->right = 0;
-	mx = mx / 13;
+	mx = mx / 15;
 	if (x != 0)
 	{
 		if ((mx >= 0 && mx < 72) && (my >= 0 && my < 670) && (x - mx) < 0)
@@ -31,9 +43,6 @@ int	mouse_move(int mx, int my, t_game *game)
 		if ((mx >= 0 && mx < 72) && (my >= 0 && my < 670) && (x - mx) > 0)
 			rotation(game, 'r');
 	}
-	// printf("my = %i mx = %i\n", my, mx);
 	x = mx;
-	return (0);
-
 	return (0);
 }
